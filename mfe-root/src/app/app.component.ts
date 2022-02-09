b3b0f7f2ee6c3d5e7532f107d942383a7a15133f
Abdullah Pariyani <abdullah.pariyani@zymr.com>
2022-02-09 14:14:53 +0530

    change prod to local

--- mfe-root/src/app/app.component.ts
+++ mfe-root/src/app/app.component.ts
@@ -9,12 +9,12 @@ export class AppComponent {
   title = 'mfe-root';
 
   // local
-  // MFE_USER_LIST = 'http://localhost:9001/main.js';
-  // MFE_USER_FORM = 'http://localhost:9002/main.js';
+  MFE_USER_LIST = 'http://localhost:9001/main.js';
+  MFE_USER_FORM = 'http://localhost:9002/main.js';
 
   // prod
-  MFE_USER_LIST = 'https://mfe-user-list.web.app/main.js';
-  MFE_USER_FORM = 'https://mfe-user-form.web.app/main.js';
+  // MFE_USER_LIST = 'https://mfe-user-list.web.app/main.js';
+  // MFE_USER_FORM = 'https://mfe-user-form.web.app/main.js';
   counter = 0;
 
   constructor() {
